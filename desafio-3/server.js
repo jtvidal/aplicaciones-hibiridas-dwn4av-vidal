66f39a15a5c03e78bffc07055108ea2315ea096f
jvidal <javier.vidal@davinci.edu.ar>
2024-09-03 20:10:40 -0300

    info and alumnos url added

--- desafio-3/server.js
+++ desafio-3/server.js
@@ -1,3 +1,24 @@
 import "dotenv/config";
+import os from "os";
+import http from "http";
+import fs from "fs/promises";
 
-const port = process.env.PORT;
+const server = http.createServer((req, res) => {
+  console.log("url: ", req.url);
+  console.log('method: ', req.method);
+  switch(req.url){
+    case '/alumno':
+        res.writeHead(200, 'text/plain')
+        res.end('Vidal Javier');
+        
+    case '/info':
+        res.writeHead(200,'text/plain')
+        res.end(`Datos del sistema:
+             Arquitectura: ${os.arch}\n
+             Hostname: ${os.hostname}`);
+  }
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`server port: ${process.env.PORT}`);
+});
