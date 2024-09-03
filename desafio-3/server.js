385c23b6db104121adec7f82236265ff69b81e97
jvidal <javier.vidal@davinci.edu.ar>
2024-09-03 20:47:41 -0300

    /static added

--- desafio-3/server.js
+++ desafio-3/server.js
@@ -3,19 +3,32 @@ import os from "os";
 import http from "http";
 import fs from "fs/promises";
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   console.log("url: ", req.url);
-  console.log('method: ', req.method);
-  switch(req.url){
-    case '/alumno':
-        res.writeHead(200, 'text/plain')
-        res.end('Vidal Javier');
-        
-    case '/info':
-        res.writeHead(200,'text/plain')
-        res.end(`Datos del sistema:\n
+  console.log("method: ", req.method);
+  switch (req.url) {
+    case "/alumno":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Vidal Javier");
+      break;
+
+    case "/info":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(`Datos del sistema:\n
              Arquitectura: ${os.arch}\n
              CPUS: ${JSON.stringify(os.cpus())}\n`);
+      break;
+    case "/static":
+      try {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        const data = await fs.readFile("index.html", "utf-8");
+        res.end(data);
+      } catch (error) {
+        console.error("Error getting file: ", error.message);
+      }
+      break;
+      default :
+      break;
   }
 });
 
