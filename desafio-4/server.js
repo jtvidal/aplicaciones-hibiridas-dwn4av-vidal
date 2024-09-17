c8c7c5ef91336166535aabbd8482120c02c7b7d7
jvidal <javier.vidal@davinci.edu.ar>
2024-09-17 20:26:41 -0300

    feat: parte 1

--- desafio-4/server.js
+++ desafio-4/server.js
@@ -1,12 +1,23 @@
 import express from "express";
-import 'dotenv/config';
+import "dotenv/config";
 
 const server = express();
-server.get('/',(req,res)=>{
-    res.send('Javier Vidal')
-})
-server.listen(process.env.PORT,()=>{
-    console.log(`Server running at: http://localhost:${process.env.PORT}`);
-})
+
+server.get("/", (req, res) => {
+  res.status(200).send("Javier Vidal");
+});
+server.get("/materia", (req, res) => {
+  res.status(200).send("Aplicaciones Híbridas");
+});
+server.get("/profesora", (req, res) => {
+  res.status(200).send("Camila Belén Marcos Galbán");
+});
+server.listen(process.env.PORT, () => {
+  console.log(`Server running at: http://localhost:${process.env.PORT}`);
+});
 
 
+// Middleware genérico: para rutas no definidas
+server.use((req, res) => {
+  res.status(404).send("Oops: page not found!");
+});
