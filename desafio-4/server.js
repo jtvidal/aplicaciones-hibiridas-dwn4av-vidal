7cd5abfccf8e323cff44064dbd9848c5d3b3e0e4
jvidal <javier.vidal@davinci.edu.ar>
2024-09-22 17:16:38 -0300

    feat: get of product by id

--- desafio-4/server.js
+++ desafio-4/server.js
@@ -3,57 +3,15 @@ import "dotenv/config";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-// const movies = [
-//   {
-//     year: "1994",
-//     title: "Pulp Fiction",
-//     director: "Quentin Tarantino",
-//     photographyDirector: "Robert Richardson",
-//     country: "USA",
-//     actors: "John Travolta, Uma Thurman, Samuel L. Jackson",
-//   },
-//   {
-//     year: "1999",
-//     title: "The Matrix",
-//     director: "Lana Wachowski, Lilly Wachowski",
-//     photographyDirector: "Bill Pope",
-//     country: "USA",
-//     actors: "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss",
-//   },
-//   {
-//     year: "2008",
-//     title: "The Dark Knight",
-//     director: "Christopher Nolan",
-//     photographyDirector: "Wally Pfister",
-//     country: "USA",
-//     actors: "Christian Bale, Heath Ledger, Aaron Eckhart",
-//   },
-//   {
-//     year: "2010",
-//     title: "Inception",
-//     director: "Christopher Nolan",
-//     photographyDirector: "Wally Pfister",
-//     country: "USA",
-//     actors: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
-//   },
-//   {
-//     year: "2014",
-//     title: "Interstellar",
-//     director: "Christopher Nolan",
-//     photographyDirector: "Hoyte van Hoytema",
-//     country: "USA",
-//     actors: "Matthew McConaughey, Anne Hathaway, Jessica Chastain",
-//   },
-// ];
+
 const server = express();
 server.use(express.json());
 
-//Esto siguiente para poder utilizar __dirname en ES6
-const __filename = fileURLToPath(import.meta.url)
+//Esto para poder utilizar __dirname en ES6
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const dataURl = path.join(__dirname, "./data.json")
+const dataURl = path.join(__dirname, "./data.json");
 const data = JSON.parse(fs.readFileSync(dataURl));
-
 
 server.get("/", (req, res) => {
   res.status(200).send("Javier Vidal");
@@ -67,21 +25,32 @@ server.get("/profesora", (req, res) => {
   res.status(200).send("Camila Belén Marcos Galbán");
 });
 
+server.get("/peliculas", (req, res) => {
+  res.status(200).json(data.movies);
+});
+
 server.get("/peliculas/:title", (req, res) => {
   let filteredMovies = [];
   filteredMovies = data.movies.filter((m) =>
     m.title.toLowerCase().includes(req.params.title.toLowerCase())
   );
   if (filteredMovies.length >= 0) {
-    console.log("pelis filtradas: ", filteredMovies);
     res.status(200).json(filteredMovies);
   } else {
     res.status(400).send("Movie not found");
   }
 });
 
-server.get('/productos', (req,res)=>{
+server.get("/productos", (req, res) => {
+  res.status(200).json(data.products);
+});
 
+server.get("/productos/:id", (req, res) => {
+  const product = data.products.find((p) => p.id === parseInt(req.params.id));
+  if (!product) {
+    res.status(400).json({ message: "Product not found" });
+  }
+  res.status(200).json({ product });
 });
 
 server.listen(process.env.PORT, () => {
@@ -90,5 +59,5 @@ server.listen(process.env.PORT, () => {
 
 // Middleware genérico: para rutas no definidas
 server.use((req, res) => {
-  res.status(404).json({ message: "Oops: page not found!" });
+  res.status(404).send("Oops: page not found!");
 });
