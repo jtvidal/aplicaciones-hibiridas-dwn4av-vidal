cdda992217c424945c9bf618ce7d0d235b100c0e
jvidal <javier.vidal@davinci.edu.ar>
2024-10-22 13:56:57 -0300

    feat: added auth middleware to a few authors and books routes

--- desafio-5/routes/authorsRoutes.js
+++ desafio-5/routes/authorsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import AuthorsController from "../controllers/authorsController.js";
+import { auth } from "../middlewares/auth.js";
 
 const authorsRouter = express.Router();
-authorsRouter.get("/", AuthorsController.getAuthors);
-authorsRouter.get("/:id", AuthorsController.getAuthorById);
+authorsRouter.get("/",auth, AuthorsController.getAuthors);
+authorsRouter.get("/:id",auth, AuthorsController.getAuthorById);
 authorsRouter.post("/", AuthorsController.createAuthor);
 authorsRouter.put("/:id", AuthorsController.editAuthor);
 authorsRouter.delete("/:id", AuthorsController.deleteAuthor);
--- desafio-5/routes/booksRoutes.js
+++ desafio-5/routes/booksRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import booksController from "../controllers/booksController.js";
+import { auth } from "../middlewares/auth.js";
 
 const booksRouter = express.Router();
-booksRouter.get("/", booksController.getBooks);
-booksRouter.get("/:id", booksController.getBooksById)
+booksRouter.get("/",auth, booksController.getBooks);
+booksRouter.get("/:id",auth, booksController.getBooksById)
 booksRouter.post('/',booksController.createBook);
 booksRouter.put('/:id', booksController.editBook);
 booksRouter.delete('/:id', booksController.deleteBook);
