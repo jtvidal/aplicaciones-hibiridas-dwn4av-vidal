9a8e472b7bf3a6a67499e41ebd9b0dc51b38edc2
jvidal <javier.vidal@davinci.edu.ar>
2024-10-28 09:48:47 -0300

    feat: users CRUD

--- desafio-8/controllers/users-controller.js
+++ desafio-8/controllers/users-controller.js
@@ -30,11 +30,53 @@ class UsersController {
     }
   }
 
-  // async getUserById(req, res, next) {
-  //   try {
+  async getUserById(req, res, next) {
+    try {
+      const userId = req.params.id;
+      const user = await Users.findOne({ _id: userId });
+      if (!user) {
+        res.status(404).json({ error: "User not found" });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+      next(error);
+    }
+  }
 
-  //   } catch (error) {}
-  // }
+  async updateUser(req, res, next) {
+    try {
+      const userId = req.params.id;
+      const data = req.body;
+      const updatedUser = await Users.findOneAndUpdate({ _id: userId }, data, {
+        new: true,
+      });
+      if (!updatedUser) {
+        res.status(404).json({ error: "Could not update user" });
+      }
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+      next(error);
+    }
+  }
+
+  async deleteUser(req, res, next) {
+    try {
+      const userId = req.params.id;
+      const deletedUser = await Users.findOneAndDelete(
+        { _id: userId },
+        { includeResultMetadata: true }
+      );
+      if (!deletedUser) {
+        res.status(404).json({ error: "Could not delete user" });
+      }
+      res.status(200).json({ deleted: deletedUser });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+      next(error);
+    }
+  }
 }
 
 export default new UsersController();
--- desafio-8/routes/users-routes.js
+++ desafio-8/routes/users-routes.js
@@ -4,5 +4,8 @@ import usersController from "../controllers/users-controller.js";
 const router = express.Router();
 router.post("/", usersController.createUser);
 router.get("/", usersController.getUsers);
+router.get("/:id", usersController.getUserById);
+router.put("/:id", usersController.updateUser);
+router.delete("/:id", usersController.deleteUser);
 
 export default router;
